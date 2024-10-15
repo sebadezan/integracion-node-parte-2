d83ced816c4677c465ad7902d48ed42a3e27a4db
Sebastian De Zan <sebastianagust7@gmail.com>
2024-10-15 12:52:06 -0300

    first commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var adminRouter = require('./routes/admin/novedades')
 var app = express();
 
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -25,6 +25,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret:'12d24jflqe3dlrd98eq7',
+  resave: false,
+  saveUninitialized: true
+}))
 
 secured = async (req, res, next) => {
   try {
@@ -46,18 +52,14 @@ app.use('/admin/login', loginRouter);
 app.use('/admin/novedades', secured, adminRouter);
 
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('admin/login', {
+    res.render('admin/novedades', {
         layout: 'admin/layout',
         persona: req.session.nombre
     });
